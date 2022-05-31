5be6c1aa76287f40a36fabfd88842fd1a1b2e164
Sanyaur <gs.ujvari@gmail.com>
2022-05-31 15:57:11 +0200

    body items imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import DetailsBody from "./components/profile/DetailsBody";
+import PersonList from "./components/sidebar/PersonList";
 
 function App() {
+  console.log(
+    fetch("http://localhost:8080/api/people").then((res) =>
+      res.json().then((data) => console.log(data))
+    )
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <PersonList />
+      <DetailsBody />
+    </Container>
   );
 }
 
