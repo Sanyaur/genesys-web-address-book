fc8dd90bac8ba6de8f065f379776084584365c2d
Sanyaur <gs.ujvari@gmail.com>
2022-06-01 13:12:42 +0200

    real data added & users now generating alphabetically

--- src/components/sidebar/PersonList.js
+++ src/components/sidebar/PersonList.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Col } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 import styled from "styled-components";
+import UsersProvider from "../../store/user-context";
 
 const PersonList = () => {
+  const users = useContext(UsersProvider);
+
+  // sorting users alphabetically, also modifying the original array
+  if (users) {
+    users.sort((a, b) => a.name > b.name);
+  }
+
   return (
     <Col md={2}>
       <Sidebar>
-        <div>lorem</div>
-        <div>lorem</div>
-        <div>lorem</div>
-        <div>lorem</div>
-        <div>lorem</div>
-        <div>lorem</div>
-        <div>lorem</div>
-        <div>lorem</div>
-        <div>lorem</div>
-        <div>lorem</div>
+        <UserList>
+          {users &&
+            users.map((user) => (
+              <UserListItem key={uuidv4()}>{user.name}</UserListItem>
+            ))}
+        </UserList>
       </Sidebar>
     </Col>
   );
@@ -24,6 +29,17 @@ const PersonList = () => {
 const Sidebar = styled.div`
   border-right: 2px black solid;
   height: 100vh;
+`;
+
+const UserList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
+const UserListItem = styled.li`
+  &:hover {
+    background-color: #eef;
+    cursor: pointer;
+  }
 `;
 
 export default PersonList;
