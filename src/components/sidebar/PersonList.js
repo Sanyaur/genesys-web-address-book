69eb8c2d26a819b1bf9f05fa7a9ddc258419fdfc
Sanyaur <gs.ujvari@gmail.com>
2022-06-06 14:27:00 +0200

    added loader & dispatch function & async used for API

--- src/components/sidebar/PersonList.js
+++ src/components/sidebar/PersonList.js
@@ -1,26 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Col } from "react-bootstrap";
 import { v4 as uuidv4 } from "uuid";
 import styled from "styled-components";
+import UsersProvider from "../../store/user-context";
+
+import { DotSpinner } from "@uiball/loaders";
+
 const PersonList = () => {
   const [users, setUsers] = useState();
+  const { state, dispatch } = useContext(UsersProvider);
 
   useEffect(() => {
     const sortedUsers = (users) => users.sort((a, b) => a.name > b.name);
 
-    fetch("http://localhost:8080/api/people").then((res) =>
-      res.json().then((data) => setUsers(sortedUsers(data.people)))
-    );
+    const fetchingData = async () => {
+      const response = await fetch("http://localhost:8080/api/people");
+      const data = await response.json();
+      setUsers(sortedUsers(data.people));
+      await dispatch({ type: "CHOSEN_USER", payload: data.people });
+    };
+    fetchingData();
   }, []);
 
+  const zeroPadding = { paddingLeft: 0, paddingRight: 0 };
   return (
-    <Col md={2}>
+    <Col md={2} style={zeroPadding}>
       <Sidebar>
         <UserList>
-          {users &&
+          {users ? (
             users.map((user) => (
               <UserListItem key={uuidv4()}>{user.name}</UserListItem>
-            ))}
+            ))
+          ) : (
+            <DotSpinner size={40} speed={0.9} color='black' />
+          )}
         </UserList>
       </Sidebar>
     </Col>
