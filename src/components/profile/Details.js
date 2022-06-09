b9e1b40f8b9d0ec951bc6163514b68986ec6a0bb
Sanyaur <gs.ujvari@gmail.com>
2022-06-09 09:34:50 +0200

    context added, component structure created

--- src/components/profile/Details.js
+++ src/components/profile/Details.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import userContext from "../../store/user-context";
+import { v4 as uuidv4 } from "uuid";
 
 function Details() {
+  const {
+    state: { chosenUser },
+  } = useContext(userContext);
+
+  const { education, workExperience } = chosenUser;
+  // const { institution, startYear, endYear, degree } = education;
+
+  console.log(chosenUser);
   return (
     <>
-      <div>Education</div>
-      <div>Experience</div>
+      <h4>Education</h4>
+      <div>
+        {education &&
+          education.map((item) => (
+            <div key={uuidv4()}>
+              <div>
+                {`${item.startYear}-${item.endYear} `}
+                {item.institution} ({item.degree})
+              </div>
+            </div>
+          ))}
+      </div>
+      <h4>Experience</h4>
+      <div>
+        {workExperience &&
+          workExperience.map((item) => (
+            <div key={uuidv4()}>
+              <div>
+                {`${item.startYear}-${item.endYear ?? "Present"} `}
+                {item.institution} ({item.title})
+              </div>
+            </div>
+          ))}
+      </div>
     </>
   );
 }
